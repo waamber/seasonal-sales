818b0ccf35acc95388d4b9c4f5e9860d6bd2a78e
Waamber <amberastuart@aol.com>
2017-08-28 23:10:52 -0500

    combined json arrays and printed to dom

--- seasonal.js
+++ seasonal.js
@@ -1,41 +1,65 @@
-function domString(products){
-	var productString = "";
-	for(var i = 0; i < products.length; i ++){
-		productString += `<div class="productsCard">
-												<h1>${products[i].name}</h1>
-												<h3>${products[i].price}</h3>
-											</div>`;
-	writeToDom(productString);
-	}
-}; 
-
-function writeToDom(strang){
-	var productContainer = document.getElementById("products-container");
-	productContainer.innerHTML = strang; 
-};
-
 function loadProducts(){
-	var productsData = JSON.parse(this.responseText); //responseText gives you a string .parse turns it into JSON array
-	domString(productsData.products);
+	var productsData = JSON.parse(this.responseText).products; 
+	getCategories(productsData);
 };
 
 function error() {
 	console.log("AHHHHH");
 };
 
-function loadCategories(){
-	var categoriesData = JSON.parse(this.responseText); //responseText gives you a string .parse turns it into JSON array
-	domString(categoriesData.categories);
-};
-
-var categoryRequest = new XMLHttpRequest();
-categoryRequest.addEventListener("load", loadCategories);
-categoryRequest.addEventListener("error", error);
-categoryRequest.open("GET", "categories.json");
-categoryRequest.send();
-
-var productsRequest = new XMLHttpRequest();
+var productsRequest = new XMLHttpRequest;
 productsRequest.addEventListener("load", loadProducts);
 productsRequest.addEventListener("error", error);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
+
+function getCategories(productsData){
+	var categoryRequest = new XMLHttpRequest;
+	categoryRequest.addEventListener("load", loadCategories);
+	categoryRequest.addEventListener("error", error);
+	categoryRequest.open("GET", "categories.json");
+	categoryRequest.send();
+	
+	function loadCategories(){
+		var categoriesData = JSON.parse(this.responseText).categories;
+		combineArrays(productsData, categoriesData);
+	}
+};
+
+function combineArrays(productsArray, categoriesArray){
+	productsArray.forEach(function(product){
+		var currentCategoryId = product.category_id;
+		categoriesArray.forEach(function(categories){
+			if(currentCategoryId === categories.id){
+				product["department"] = categories.name;
+				product["finalPrice"] = product.price - categories.discount;
+				product["season"] = categories.season_discount;
+			}
+		});
+	});
+	console.log("products", productsArray);
+	domString(productsArray);
+}
+
+function domString(products){
+	var productString = "";
+	for(var i = 0; i < products.length; i ++){
+		productString += `<div class="productCard">
+												<h2>${products[i].name}</h2>
+												<h3>${products[i].department}</h3>
+												<div class= "productImg"><img src ="${products[i].url}"></div>
+												<h4>${products[i].finalPrice}</h4>
+									</div>`;
+	}
+	writeToDom(productString);
+};
+
+function writeToDom(product){
+		var productsContainer = document.getElementById("products-container");
+		productsContainer.innerHTML = product; 
+};
+
+
+
+
+
